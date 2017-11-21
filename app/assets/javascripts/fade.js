c4756bbc62a81a8360fec04e992ffb51e0526f35
ralph kemp <ralphk3mp@gmail.com>
2017-11-21 03:50:10 +0000

    going to bed

--- app/assets/javascripts/fade.js
+++ app/assets/javascripts/fade.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-  $(".navbar-wagon-link").each(function(i) {
-    $(this).delay(200 * i).fadeIn(1000);
-  });
-});
+// $(document).ready(function() {
+//   $(".navbar-wagon-link").each(function(i) {
+//     $(this).delay(200 * i).fadeIn(900);
+//   });
+// });
+
 
 
