89c0ad189ae698cfcfa0c2162ce07a1d1fa4afd2
ralph kemp <ralphk3mp@gmail.com>
2017-11-21 03:22:38 +0000

    changed to attachinary instead

---
+++ db/migrate/20171121024338_create_attachinary_tables.attachinary.rb
@@ -0,0 +1,16 @@
+class CreateAttachinaryTables < ActiveRecord::Migration[5.0]
+  def change
+    create_table :attachinary_files do |t|
+      t.references :attachinariable, polymorphic: true, index: false
+      t.string :scope
+      t.string :public_id
+      t.string :version
+      t.integer :width
+      t.integer :height
+      t.string :format
+      t.string :resource_type
+      t.timestamps
+    end
+    add_index :attachinary_files, [:attachinariable_type, :attachinariable_id, :scope], name: 'by_scoped_parent'
+  end
+end
