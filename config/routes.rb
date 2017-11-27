0b529d2722cc78ef4bd4be1f340b83a3fed394ce
ralph kemp <ralphk3mp@gmail.com>
2017-11-27 18:18:45 +0000

    whatever

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   devise_for :users, controllers: { registrations: "registrations"}
-  root to: 'pages#home'
+  root to: 'projects#index'
   resources :projects
   get 'about', to: 'pages#about'
   get 'music', to: 'pages#music'
